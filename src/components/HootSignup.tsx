360efd6ba6ad10b354b1cf1417d7d2d2a4f3b6ae
Ganondorc <michaelfrake2015@gmail.com>
2022-03-29 20:47:31 -0500

    template for login

--- src/components/HootSignup.tsx
+++ src/components/HootSignup.tsx
@@ -9,7 +9,8 @@ export default function HootSignup() {
 
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
 
-        event.preventDefault(); const data = new FormData(event.currentTarget);
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
         console.log({
             email: data.get('email'),
             password: data.get('password'),
