27eba65800035a1d7816c66910183c1da17d2549
nanaboakye123 <101526161+nanaboakye123@users.noreply.github.com>
2022-04-02 16:48:13 -0500

    Add files via upload

---
+++ src/components/HootHome.test.tsx
@@ -0,0 +1,22 @@
+import {render, screen, cleanup} from '@testing-library/react'
+import '@testing-library/jest-dom'
+import HootHome from "../components/HootHome";
+
+describe("HootHome Component Test", () => {
+    test('There should be one NavBar rendered', () => {
+      // to be added        
+    });
+
+    
+    test("Should contain picture of Mr.Owl", () => {
+       // to be added
+    });
+
+    test("Links for (Home, About, Users) should exist and lead to correct pages", () => {
+        // To be added
+    });
+
+    test("Is able to be clicked/ allows for user interaction", () => {
+        // To be added
+    });
+});
