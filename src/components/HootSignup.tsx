9fd65099eff89275ae9b2a728d543338cce4124d
Brian Lucas <bcl1713@gmail.com>
2022-04-03 13:02:01 +0200

    refactors signup form and states

--- src/components/HootSignup.tsx
+++ src/components/HootSignup.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Avatar, Box, Button, Container, Grid, Link, TextField, Typography } from '@mui/material';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import { useNavigate } from 'react-router-dom';
@@ -7,13 +7,23 @@ export default function HootSignup() {
 
     const navigate = useNavigate();
 
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [verifyPassword, setVerifyPassword] = useState("");
+        
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
 
         event.preventDefault();
-        const data = new FormData(event.currentTarget);
         console.log({
-            email: data.get('email'),
-            password: data.get('password'),
+            firstName,
+            lastName,
+            username,
+            email,
+            password,
+            verifyPassword,
         });
         navigate("../", { replace: true });
     };
@@ -37,6 +47,9 @@ export default function HootSignup() {
                     <Grid container spacing={2}>
                         <Grid item xs={12} sm={6}>
                             <TextField
+                                onChange = {(event) => {
+                                    setFirstName(event.target.value);
+                                }}
                                 required
                                 fullWidth
                                 id="firstName"
@@ -48,6 +61,9 @@ export default function HootSignup() {
                         </Grid>
                         <Grid item xs={12} sm={6}>
                             <TextField
+                                onChange = {(event) => {
+                                    setLastName(event.target.value);
+                                }}
                                 required
                                 fullWidth
                                 id="lastName"
@@ -58,6 +74,22 @@ export default function HootSignup() {
                         </Grid>
                         <Grid item xs={12}>
                             <TextField
+                                onChange = {(event) => {
+                                    setUsername(event.target.value);
+                                }}
+                                required
+                                fullWidth
+                                id="username"
+                                label="User Name"
+                                name="username"
+                                autoComplete="username"
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                onChange = {(event) => {
+                                    setEmail(event.target.value);
+                                }}
                                 required
                                 fullWidth
                                 id="email"
@@ -68,12 +100,29 @@ export default function HootSignup() {
                         </Grid>
                         <Grid item xs={12}>
                             <TextField
+                                onChange = {(event) => {
+                                    setPassword(event.target.value);
+                                }}
                                 required
                                 fullWidth
                                 name="password"
                                 label="Password"
                                 type="password"
                                 id="password"
+                                autoComplete="new-password"
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                onChange = {(event) => {
+                                    setVerifyPassword(event.target.value);
+                                }}
+                                required
+                                fullWidth
+                                name="verifyPassword"
+                                label="Verify Password"
+                                type="password"
+                                id="verifyPassword"
                                 autoComplete="new-password"
                             />
                         </Grid>
