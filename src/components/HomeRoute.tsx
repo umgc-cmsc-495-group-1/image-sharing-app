7eb851ef90b1f10185456f32f73ee986fbe90d34
Brian Lucas <bcl1713@gmail.com>
2022-04-20 21:11:49 +0200

    fixed protected routes

--- src/components/HomeRoute.tsx
+++ src/components/HomeRoute.tsx
@@ -1,13 +1,14 @@
-import { User } from "firebase/auth";
 import React, { useContext } from "react";
 import { Navigate } from "react-router-dom";
 import { FirebaseAuthContext } from "../context/AuthContext";
 
 export default function HomeRoute({ children }: { children: JSX.Element }) {
-  const user: User | null = useContext(FirebaseAuthContext);
+  const authValue = useContext(FirebaseAuthContext);
 
-  if (!user) {
-    return <Navigate to="/home" replace />;
+  if (authValue?.userDataPresent) {
+    if (authValue.user) {
+      return <Navigate to="/home" replace />;
+    }
   }
   return children;
 }
