bc993c9e22554e9e4a90d04d66c6b6302f903561
Brian Lucas <bcl1713@gmail.com>
2022-04-22 21:52:18 +0200

    add image selector button

--- src/components/UploadImage.tsx
+++ src/components/UploadImage.tsx
@@ -1,12 +1,46 @@
 import { CloudUploadOutlined } from "@mui/icons-material";
-import { Avatar, Box, Container, Typography } from "@mui/material";
-import React from "react";
+import {
+  Avatar,
+  Box,
+  Button,
+  Container,
+  Grid,
+  Typography,
+} from "@mui/material";
+import React, { useState } from "react";
 
 export default function UploadImage() {
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     //TODO implement
+    //console.log(state);
     console.log(event);
     return;
+  };
+
+  interface State {
+    currentFile: File | null;
+    previewImage: string | null;
+    progress: number;
+    isError: boolean;
+    message: string;
+    imageInfos: never[];
+  }
+
+  const [state, setState] = useState<State>({
+    currentFile: null,
+    previewImage: null,
+    progress: 0,
+    message: "",
+    isError: false,
+    imageInfos: [],
+  });
+
+  const selectFile = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setState((previous) => ({
+      ...previous,
+      currentFile: event.target.files ? event.target.files[0] : null,
+    }));
+    console.log(state);
   };
 
   return (
@@ -22,6 +56,7 @@ export default function UploadImage() {
         <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
           <CloudUploadOutlined />
         </Avatar>
+        <Typography>Upload Image</Typography>
         <Box
           component="form"
           noValidate
@@ -29,7 +64,14 @@ export default function UploadImage() {
           sx={{ mt: 3 }}
           role="upload-image"
         ></Box>
-        <Typography>Upload Image</Typography>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Button fullWidth variant="contained" component="label">
+              Select Image
+              <input type="file" hidden onChange={selectFile} />
+            </Button>
+          </Grid>
+        </Grid>
       </Box>
     </Container>
   );
