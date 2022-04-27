c8a6f7f6ea3d9f10911b284f03bea808b5086137
tmg7969 <sweartomod@gmail.com>
2022-04-27 14:51:26 -0400

    added profile text update function

--- src/types/appTypes.d.ts
+++ src/types/appTypes.d.ts
@@ -166,3 +166,9 @@ export type ImageItemProps = {
 export interface ProfileImageInterface extends ImageItemProps {
   username: string;
 }
+
+export interface ProfileUpdateInterface {
+  displayName: string,
+  email: string,
+  bio: string
+}
