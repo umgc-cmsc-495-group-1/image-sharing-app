8c5b219ae59b174619a5ebe863f76fd44bb708a9
Katherine Eisenhuth <101725448+katheise17913@users.noreply.github.com>
2022-04-28 20:28:55 -0500

    eisenhuth-settings-updated
    still needs update bio feature. Will add at later date.

--- src/components/HootUserSettings.tsx
+++ src/components/HootUserSettings.tsx
@@ -26,40 +26,15 @@ function HootUser() {
 
 export default HootUser;
 return(
-  <div className="class.navigation">
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-              role='menu-icon'
-              onClick={(event) => {
-                toggleDrawer(!isOpen, event);
-              }}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Box
-              component="img"
-              sx={{
-                height: 64,
-              }}
-              alt="Hoot Logo"
-              src={require("../assets/logo/png/simple-72x72.png")}
-            />
-            <Typography variant="h6">
-              {uid ? uid + " - Hoot!" : "Hoot!"}
-            </Typography>
-	<List>
-            <ListItem button component={Link} to="/" role="terms-of-service">
-              <ListItemText primary="Terms of Service" />
-                <ListItem button component={Link} to="/login" role="privacy-policy">
-                  <ListItemText primary="Privacy Policy" />
-                </ListItem>
-               
-              </>
+	const HootUserSettings: React.FC = () => {
+ 		return(
+   			<List>
+      				<ListItem button component={Link} to="/" role="terms-of-service">
+       		 		<ListItemText primary="Terms of Service" />
+      				<ListItem button component={Link} to="/login" role="privacy-policy">
+       		 		<ListItemText primary="Privacy Policy" />
+      				</ListItem>
+    			</List>
+  )
+}
 );
