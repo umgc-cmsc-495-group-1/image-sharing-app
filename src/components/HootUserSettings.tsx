be7f9659dde0d260a11a706513db1bc57139db96
Dylan <dylan.g.bryan@gmail.com>
2022-04-30 03:18:02 -0400

    Update HootUserSettings.tsx
    corrected JSX

--- src/components/HootUserSettings.tsx
+++ src/components/HootUserSettings.tsx
@@ -1,40 +1,20 @@
-import { Typography } from "@mui/material";
-import { useParams } from "react-router-dom";
-import React, { useState } from "react";
+import React from "react";
 import {
-  AppBar,
-  Box,
-  Drawer,
-  IconButton,
   List,
   ListItem,
   ListItemText,
-  Toolbar,
-  Typography,
 } from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
-import { Link, Outlet, useNavigate } from "react-router-dom";
-import { auth } from "../firebaseSetup";
-import { logout } from "../data/authFunctions";
-import { onAuthStateChanged } from "firebase/auth";
+import { Link } from "react-router-dom";
 
-function HootUser() {
-  const { userId } = useParams();
-  return <Typography>This is user {userId} settings</Typography>;
-
+const HootUserSettings: React.FC = () => {
+  return(
+    <List>
+      <ListItem button component={Link} to="/about/terms-of-service" role="terms-of-service">
+        <ListItemText primary="Terms of Service" />
+      </ListItem>
+      <ListItem button component={Link} to="/about/privacy" role="privacy-policy">
+        <ListItemText primary="Privacy Policy" />
+      </ListItem>
+    </List>
+  );
 }
-
-export default HootUser;
-return(
-	const HootUserSettings: React.FC = () => {
- 		return(
-   			<List>
-      				<ListItem button component={Link} to="/" role="terms-of-service">
-       		 		<ListItemText primary="Terms of Service" />
-      				<ListItem button component={Link} to="/login" role="privacy-policy">
-       		 		<ListItemText primary="Privacy Policy" />
-      				</ListItem>
-    			</List>
-  )
-}
-);
