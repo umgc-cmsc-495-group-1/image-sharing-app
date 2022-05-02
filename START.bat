735e5d099a4a6fb1131f41b3a2d80b3802a82cb0
Ganondorc <michaelfrake2015@gmail.com>
2022-05-02 01:00:18 -0500

    pretty FriendsList, added remove btn & start bat

---
+++ START.bat
@@ -0,0 +1,2 @@
+start "" npm run emulators:start
+npm run start
--- src/components/Friends/index.tsx
+++ src/components/Friends/index.tsx
@@ -1,24 +1,21 @@
 import React from "react";
 import FriendsList from "./FriendsList";
 import { useFriends } from "../../hooks/useFriends";
-import { useCurrentUser } from "../../hooks/useCurrentUser";
 import { Box } from "@mui/material";
 
 const Friends: React.FC = () => {
-  const user = useCurrentUser();
   const friends = useFriends();
   return (
     <Box
     sx={{
       display: "flex",
       flexDirection: "column",
-      width: "50%",
+      alignItems: "center",
+      width: "100%",
     }}
     >
       <h2>Friends</h2>
       <FriendsList
-        uid={user.uid}
-        profilePicURL={""}
         friends={friends}
       />
     </Box>
