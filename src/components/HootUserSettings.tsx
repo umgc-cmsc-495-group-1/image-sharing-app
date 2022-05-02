824dbacff90e8a69985fa3433acca6232c480e38
Katherine Eisenhuth <101725448+katheise17913@users.noreply.github.com>
2022-05-02 13:41:46 -0500

    Update HootUserSettings.tsx

--- src/components/HootUserSettings.tsx
+++ src/components/HootUserSettings.tsx
@@ -1,43 +1,19 @@
- import React from "react";
-import {updateProfile} from "../data/userData";
+
+import {getUserByUserId} from "../data/userData";
 import React, { useState } from "react";
+import { User } from '@firebase/auth';
 import {
   List,
   ListItem,
-import {
+  TextField,
+  Button,
+  ListItemText,
 } from "@mui/material";
 import { Link } from "react-router-dom";
 
-const handleSubmit = async (event: React.MouseEvent<HTMLButtonElement>) => {
-	return (
-	
-	<TextField 
-	  fullWidth
-	  name="NewUserBio"
-	  label="Enter Updated Bio"
-	  id="UpdatedBio"
-	/>
-	
-	<Button color="primary" variant="contained" onClick={updateBio}>
-	  Update Bio
-	</Button>
-);
 const bodyStyle: React.CSSProperties = {
 	fontFamily: 'Roboto',
 	fontSize: 8
-}
-
-
-const updateBio = async (event: React.MouseEvent<HTMLButtonElement>) => {
-	return (
-	const { uid } = useParams();
-	const profileData = getProfileData(uid);
-	const Profile: UserMetaData= {
-		bio: UpdatedBio,
-		dispayName: UpdatedDisplayName,
-	};
-	
-);
 }
 
 const HootUserSettings: React.FC = () => {
@@ -45,38 +21,48 @@ const HootUserSettings: React.FC = () => {
     <>
     <List>
       <ListItem button component={Link} to="/about/terms-of-service" role="terms-of-service">
-        <ListItemText primary="Terms of Service" />
-        <ListItemText style={{bodyStyle}} primary="Terms of Service" />
+      <ListItemText primary="Terms of Service" />
+      <ListItemText style={bodyStyle} primary="Terms of Service" />
       </ListItem>
       <ListItem button component={Link} to="/about/privacy" role="privacy-policy">
-        <ListItemText primary="Privacy Policy" />
-      <Button color="primary" variant="contained" onClick={handleSubmit}>
-	Update Bio
-      </Button>
-        <ListItemText style={{bodyStyle}} primary="Privacy Policy" />
+      <ListItemText primary="Privacy Policy" />
+      <ListItemText style={bodyStyle} primary="Privacy Policy" />
       </ListItem>
       <TextField
 	fullWidth
-	style="bodyStyle"
 	name="NewDisplayName"
-	lable="Enter New Display Name"
+	label="Enter New Display Name"
 	id="UpdatedDisplayName"
       />
       <TextField 
 	  fullWidth
-	  style="bodyStyle"
 	  name="NewUserBio"
 	  label="Enter Updated Bio"
 	  id="UpdatedBio"
 	/>
      
-	<Button style={{bodyStyle}} color="primary" variant="contained" onClick={updateBio}>
+       <Button style={bodyStyle} color="primary" variant="contained" onClick={updateProfile}>
 	  Update User Settings
-	</Button>
-    </List>
-    </>
+       </Button>
+       </List>
+       </>
 
   );
 }
 
-export default HootUserSettings;
+const updateProfile = async (event: React.MouseEvent<HTMLButtonElement>) => {
+	return (
+		getUserByUserId.bio = "UpdatedBio",
+		getUserByUserId.displayName = "UpdatedDisplayName",
+	//user: AppUserInterface= {
+		//bio: UpdatedBio,
+		//dispayName: UpdatedDisplayName,
+	//}
+	
+    );
+};
+
+
+
+
+export HootUserSettings;
