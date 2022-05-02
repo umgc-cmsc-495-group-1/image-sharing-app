04cada3de17474a77161e46f5870fb29cca7f039
Katherine Eisenhuth <101725448+katheise17913@users.noreply.github.com>
2022-05-01 20:05:06 -0500

    eisenhuth-update-bio
    draft of update bio component

--- src/components/HootUserSettings.tsx
+++ src/components/HootUserSettings.tsx
@@ -3,8 +3,35 @@ import {
   List,
   ListItem,
   ListItemText,
+  Box,
+  Typography,
+  Button,
+  TextField,
 } from "@mui/material";
 import { Link } from "react-router-dom";
+
+const handleSubmit = async (event: React.MouseEvent<HTMLButtonElement>) => {
+	return (
+	
+	<TextField 
+	  fullWidth
+	  name="NewUserBio"
+	  label="Enter Updated Bio"
+	  id="UpdatedBio"
+	/>
+	
+	<Button color="primary" variant="contained" onClick={updateBio}>
+	  Update Bio
+	</Button>
+);
+
+const UpdateBio = async (event: React.MouseEvent<HTMLButtonElement>) => {
+	return (
+
+	<UserMetaData
+		bio={profileData.UpdatedBio}
+	/>
+	
 
 const HootUserSettings: React.FC = () => {
   return(
@@ -14,7 +41,9 @@ const HootUserSettings: React.FC = () => {
       </ListItem>
       <ListItem button component={Link} to="/about/privacy" role="privacy-policy">
         <ListItemText primary="Privacy Policy" />
-      </ListItem>
+      <Button color="primary" variant="contained" onClick={handleSubmit}>
+	Update Bio
+      </Button>
     </List>
   );
 }
