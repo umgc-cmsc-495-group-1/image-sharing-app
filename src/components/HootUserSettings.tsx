9473c32708ae9b5475562eca583c21cdae5bc126
Katherine Eisenhuth <101725448+katheise17913@users.noreply.github.com>
2022-05-03 10:33:14 -0500

    Update HootUserSettings.tsx

--- src/components/HootUserSettings.tsx
+++ src/components/HootUserSettings.tsx
@@ -1,5 +1,5 @@
-
 import {getUserByUserId} from "../data/userData";
+import {ProfileInterface} from "../types/appTypes";
 import React, { useState } from "react";
 import { User } from '@firebase/auth';
 import {
@@ -17,17 +17,17 @@ const bodyStyle: React.CSSProperties = {
 	fontSize: 8
 }
 
-export default function updateProfile(getUserByUserId: {user:AppUserInterface}) { //= async (event: React.MouseEvent<HTMLButtonElement>) => {
+export default function updateProfile(getUserByUserId: {User: AppUserInterface}) { //= async (event: React.MouseEvent<HTMLButtonElement>) => {
   const [bio, setBio] = useState('');
   const [displayName, setDisplayName] = useState('');
-  const updateProfile = () => {
-    bio: "updatedBio";
-    displayName: "updatedDisplayName";
-  }
   
-};
-
-const HootUserSettings: React.FC = () => {
+  const updateProfile = async (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    const User: AppUserInterface = {
+        displayName: updatedDisplayName,
+        bio: updatedBio,
+    };
+  } 
   return(
     <>
     <List>
@@ -40,20 +40,20 @@ const HootUserSettings: React.FC = () => {
       <ListItemText style={bodyStyle} primary="Privacy Policy" />
       </ListItem>
       <TextField
-	fullWidth
-	name="NewDisplayName"
-	label="Enter New Display Name"
-	id="UpdatedDisplayName"
-      />
+	      fullWidth
+	      name="NewDisplayName"
+	      label="Enter New Display Name"
+      	id="UpdatedDisplayName"
+       />
       <TextField 
-	  fullWidth
-	  name="NewUserBio"
-	  label="Enter Updated Bio"
-	  id="UpdatedBio"
-	/>
+	     fullWidth
+	     name="NewUserBio"
+	     label="Enter Updated Bio"
+	     id="UpdatedBio"
+      />
      
        <Button style={bodyStyle} color="primary" variant="contained" onClick={() => updateProfile}>
-	  Update User Settings
+	      Update User Settings
        </Button>
        </List>
        </>
