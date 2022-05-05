d83bec77554c66a736172e6a01a9c0111be8f70b
Katherine Eisenhuth <101725448+katheise17913@users.noreply.github.com>
2022-05-04 20:34:01 -0500

    Update HootUserSettings.tsx

--- src/components/HootUserSettings.tsx
+++ src/components/HootUserSettings.tsx
@@ -1,3 +1,4 @@
+import { UserInterface } from "../types/authentication";
 import {getUserByUserId} from "../data/userData";
 import {ProfileInterface} from "../types/appTypes";
 import React, { useState } from "react";
@@ -11,21 +12,45 @@ import {
 } from "@mui/material";
 import { Link } from "react-router-dom";
 import { AppUserInterface } from "../types/authentication";
+import { data } from "@tensorflow/tfjs";
+import { firestore } from "../firebaseSetup";
+import {
+  doc,
+  setDoc,
+  getDoc,
+  getDocs,
+  getDocsFromServer,
+} from "firebase/firestore";
+import "firebase/storage";
+import { query, where } from "firebase/firestore";
 
 const bodyStyle: React.CSSProperties = {
 	fontFamily: 'Roboto',
 	fontSize: 8
 }
 
-export default function updateProfile(getUserByUserId: {User: AppUserInterface}) { //= async (event: React.MouseEvent<HTMLButtonElement>) => {
+export default function updateProfile(GetUserByUserId: {User: AppUserInterface}) { //= async (event: React.MouseEvent<HTMLButtonElement>) => {
   const [bio, setBio] = useState('');
   const [displayName, setDisplayName] = useState('');
   
   const updateProfile = async (event: React.MouseEvent<HTMLButtonElement>) => {
+    const userRef = doc(firestore, "users", userId);
+    const docSnap = await getDoc(userRef);
+    const data = docSnap.data();
     event.preventDefault();
-    const User: AppUserInterface = {
+    const user: AppUserInterface = {
         displayName: updatedDisplayName,
         bio: updatedBio,
+        uid: data.uid,
+        username: data.username,
+        first: data.first,
+        last: data.last,
+        email: data.email,
+        likes: data.likes,
+        friends: data.friends,
+        interests: data.interests,
+    };
+      return user;
     };
   } 
   return(
