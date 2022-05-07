838ded499470d445e904f67372150b966d115b57
Brian Lucas <bcl1713@gmail.com>
2022-05-07 08:55:15 +0200

    standardize page width for feed and explore

--- src/components/Explore/index.tsx
+++ src/components/Explore/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { UploadFab } from "../UploadFab";
-import { Box } from "@mui/material";
+import { Box, Container } from "@mui/material";
 import { useExplore } from "../../hooks/useExplore";
 import { FeedPostType } from "../../types/appTypes";
 import Post from "../Profile/Post";
@@ -8,23 +8,26 @@ import Post from "../Profile/Post";
 const Explore = () => {
   const explore = useExplore();
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        width: "100%",
-      }}
-    >
-      <Box maxWidth="md">
-        {explore.map((item: FeedPostType) => (
-          <Post key={item.pid} pid={item.pid} />
-        ))}
+    <Container maxWidth="md">
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          width: "100%",
+        }}
+        role="explore-container"
+      >
+        <Box maxWidth="sm">
+          {explore.map((item: FeedPostType) => (
+            <Post key={item.pid} pid={item.pid} />
+          ))}
+        </Box>
+        <Box>
+          <UploadFab />
+        </Box>
       </Box>
-      <Box>
-        <UploadFab />
-      </Box>
-    </Box>
+    </Container>
   );
 };
 
--- src/components/Feed/index.tsx
+++ src/components/Feed/index.tsx
@@ -1,31 +1,33 @@
 import React from "react";
-import { Box } from "@mui/material";
+import { Box, Container } from "@mui/material";
 import { FeedPostType } from "../../types/appTypes";
 import { UploadFab } from "../UploadFab";
 import { useFeed } from "../../hooks/useFeed";
-import Post  from "../Profile/Post";
+import Post from "../Profile/Post";
 
 const Feed: React.FC = (): JSX.Element => {
   const feed = useFeed();
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        width: "100%",
-      }}
-      role="feed-container"
-    >
-      <Box maxWidth="md">
-        {feed.map((item: FeedPostType) => (
-          <Post key={item.pid} pid={item.pid} />
-        ))}
+    <Container maxWidth="md">
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          width: "100%",
+        }}
+        role="feed-container"
+      >
+        <Box maxWidth="sm">
+          {feed.map((item: FeedPostType) => (
+            <Post key={item.pid} pid={item.pid} />
+          ))}
+        </Box>
+        <Box>
+          <UploadFab />
+        </Box>
       </Box>
-      <Box>
-        <UploadFab />
-      </Box>
-    </Box>
+    </Container>
   );
 };
 
